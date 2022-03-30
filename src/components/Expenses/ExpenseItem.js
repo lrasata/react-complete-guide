1277672cf4d8a89a9542fae45054f3d35cc1cf3f
lrasata <rmliantsoa@gmail.com>
2022-03-30 21:00:17 +1300

    rendering list of data

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  //  ExpenseItem evaluated by React
-  const [title, setTitle] = useState(props.title);
-
-  const onClickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={onClickHandler}>Change title</button>
       </div>
     </Card>
   );
